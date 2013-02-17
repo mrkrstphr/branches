d832ed85dddf10b47f14e0765c078ab5534d00b8
Kristopher Wilson <kristopherwilson@gmail.com>
2013-02-17 15:46:05 -0500

    Refactoring Sources. Closes #12

--- src/Branches/Domain/Model/Source.php
+++ src/Branches/Domain/Model/Source.php
@@ -5,7 +5,6 @@
 
 namespace Branches\Domain\Model;
 
-use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
@@ -14,16 +13,32 @@ use Doctrine\Common\Collections\ArrayCollection;
 class Source extends \Branches\Domain\Model\Entity
 {
     use Noted;
+    use Timestamped;
 
     /**
      * @var string
      */
-    protected $page;
+    protected $title;
 
     /**
-     * @var DateTime
+     * @var string
      */
-    protected $dateOfEntry;
+    protected $author;
+
+    /**
+     * @var string
+     */
+    protected $publication;
+
+    /**
+     * @var string
+     */
+    protected $agency;
+
+    /**
+     * @var string
+     */
+    protected $abbr;
 
     /**
      * @var string
@@ -31,65 +46,110 @@ class Source extends \Branches\Domain\Model\Entity
     protected $text;
 
     /**
-     * @var integer
+     * @var string
      */
-    protected $certainty;
+    protected $rin;
+
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $notes;
 
     /**
      *
      */
     public function __construct()
     {
-        $this->dateOfEntry = new DateTime();
         $this->notes = new ArrayCollection();
     }
 
     /**
-     * @param int $certainty
+     * @param string $abbr
      */
-    public function setCertainty($certainty)
+    public function setAbbr($abbr)
     {
-        $this->certainty = $certainty;
-    }
-
-    /**
-     * @return int
-     */
-    public function getCertainty()
-    {
-        return $this->certainty;
-    }
-
-    /**
-     * @param DateTime $dateOfEntry
-     */
-    public function setDateOfEntry(DateTime $dateOfEntry)
-    {
-        $this->dateOfEntry = $dateOfEntry;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getDateOfEntry()
-    {
-        return $this->dateOfEntry;
-    }
-
-    /**
-     * @param string $page
-     */
-    public function setPage($page)
-    {
-        $this->page = $page;
+        $this->abbr = $abbr;
     }
 
     /**
      * @return string
      */
-    public function getPage()
+    public function getAbbr()
     {
-        return $this->page;
+        return $this->abbr;
+    }
+
+    /**
+     * @param string $agency
+     */
+    public function setAgency($agency)
+    {
+        $this->agency = $agency;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAgency()
+    {
+        return $this->agency;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+
+    /**
+     * @param string $publication
+     */
+    public function setPublication($publication)
+    {
+        $this->publication = $publication;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublication()
+    {
+        return $this->publication;
+    }
+
+    /**
+     * @param string $rin
+     */
+    public function setRin($rin)
+    {
+        $this->rin = $rin;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRin()
+    {
+        return $this->rin;
     }
 
     /**
@@ -106,5 +166,21 @@ class Source extends \Branches\Domain\Model\Entity
     public function getText()
     {
         return $this->text;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
     }
 }
--- tests/unit/Branches/Domain/Model/SourceTest.php
+++ tests/unit/Branches/Domain/Model/SourceTest.php
@@ -25,17 +25,26 @@ class SourceTest extends \PHPUnit_Framework_TestCase
      */
     public function testSource()
     {
-        $dateOfEntry = new \DateTime('1985-12-06 11:33:06');
-
         $source = new Source();
-        $source->setCertainty(4);
-        $source->setDateOfEntry($dateOfEntry);
-        $source->setPage('Book 3 Page 14');
-        $source->setText('TEST test Test teST');
+        $source->setTitle('History of Kent County, Michigan');
+        $source->setAuthor('Chas. C. Chapman');
+        $source->setPublication('Chicago, 1881');
+        $source->setAgency('Public Domain');
+        $source->setRin(4);
+        $source->setAbbr('Something');
+        $source->setText('This is some text');
 
-        $this->assertEquals(4, $source->getCertainty());
-        $this->assertEquals($dateOfEntry, $source->getDateOfEntry());
-        $this->assertEquals('Book 3 Page 14', $source->getPage());
-        $this->assertEquals('TEST test Test teST', $source->getText());
+        $note = new Note();
+        $source->getNotes()->add($note);
+
+        $this->assertEquals('History of Kent County, Michigan', $source->getTitle());
+        $this->assertEquals('Chas. C. Chapman', $source->getAuthor());
+        $this->assertEquals('Chicago, 1881', $source->getPublication());
+        $this->assertEquals('Public Domain', $source->getAgency());
+        $this->assertEquals(4, $source->getRin());
+        $this->assertEquals('This is some text', $source->getText());
+        $this->assertEquals('Something', $source->getAbbr());
+        $this->assertCount(1, $source->getNotes());
+
     }
 }
