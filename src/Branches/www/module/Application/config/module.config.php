dad03357729788dbb2bab7a5cc05c588d126c906
Kristopher Wilson <kristopherwilson@gmail.com>
2013-02-26 18:55:42 -0500

    use Zend\Navigation to generate the page menus

--- src/Branches/www/module/Application/config/module.config.php
+++ src/Branches/www/module/Application/config/module.config.php
@@ -47,6 +47,7 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/menu'           => __DIR__ . '/../view/layout/partial/menu.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
