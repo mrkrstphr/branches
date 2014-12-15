b0ef9dc3ae394ba912fab606707accacdb074010
Kristopher Wilson <kristopherwilson@gmail.com>
2014-12-14 23:28:52 -0500

    fleshing out Place entity

--- src/Domain/Entity/Place/Place.php
+++ src/Domain/Entity/Place/Place.php
@@ -3,6 +3,7 @@
 namespace Branches\Domain\Entity\Place;
 
 use Branches\Domain\Entity\AbstractEntity;
+use Branches\Domain\Entity\TimestampedTrait;
 
 /**
  * Class Place
@@ -10,14 +11,57 @@ use Branches\Domain\Entity\AbstractEntity;
  */
 class Place extends AbstractEntity
 {
+    use TimestampedTrait;
+
     /**
      * @var string
      */
     protected $description;
 
+    /**
+     * @var string
+     */
     protected $address1;
 
+    /**
+     * @var
+     */
     protected $address2;
+
+    /**
+     * @var string
+     */
+    protected $city;
+
+    /**
+     * @var string
+     */
+    protected $stateProvince;
+
+    /**
+     * @var string
+     */
+    protected $country;
+
+    /**
+     * @var string
+     */
+    protected $postalCode;
+
+    /**
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * @var float
+     */
+    protected $latitude;
+
+    /**
+     * @var float
+     */
+    protected $longitude;
 
     /**
      * @return string
@@ -35,6 +79,150 @@ class Place extends AbstractEntity
     {
         $this->description = $description;
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress1()
+    {
+        return $this->address1;
+    }
+
+    /**
+     * @param string $address1
+     */
+    public function setAddress1($address1)
+    {
+        $this->address1 = $address1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress2()
+    {
+        return $this->address2;
+    }
+
+    /**
+     * @param mixed $address2
+     */
+    public function setAddress2($address2)
+    {
+        $this->address2 = $address2;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStateProvince()
+    {
+        return $this->stateProvince;
+    }
+
+    /**
+     * @param string $stateProvince
+     */
+    public function setStateProvince($stateProvince)
+    {
+        $this->stateProvince = $stateProvince;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param string $postalCode
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
     }
 
     /**
