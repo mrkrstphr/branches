0e19dcbf98a131d07257d8ecd4c5b9cc0cd245c4
Kristopher Wilson <kristopherwilson@gmail.com>
2015-01-01 10:56:23 -0500

    adding jshint linter

--- gulp/tasks/development.js
+++ gulp/tasks/development.js
@@ -3,6 +3,6 @@
 var config = require('../config');
 var gulp = require('gulp');
 
-gulp.task('dev', ['server', 'browser-sync'], function() {
-  return gulp.watch(config.watch.paths, ['reload']);
+gulp.task('dev', ['lint', 'server', 'browser-sync'], function() {
+  return gulp.watch(config.watch.paths, ['lint', 'reload']);
 });
